b8d2f02af426012acf03fab74431dae591268513
potapovDim <potapovd3012@gmail.com>
2017-10-17 20:54:56 +0300

    base set up

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  entry: './src/ui/main.js',
+  output: {
+    path: './',
+    filename: "index.js"
+  },
+  devServer: {
+    port: 7070,
+    historyApiFallback: true
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015', 'stage-0'],
+        }
+      }
+    ]
+  }
+}
