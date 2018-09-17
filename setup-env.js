94cfb6fb1902c039fe8942dae11ee14f3a94dd35
potapovDim <potapovd3012@gmail.com>
2018-09-17 12:32:01 +0300

    change set-up

--- setup-env.js
+++ setup-env.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 ":" //; exec /usr/bin/env node
 
 const copyDir = require('copy-dir')
@@ -5,7 +6,9 @@ const path = require('path')
 const fs = require('fs')
 const reportDir = path.resolve(__dirname, './reporter')
 
-function executeCopyDir(reportPath) {copyDir.sync(path.resolve(process.cwd(), reportPath), reportDir)}
+function executeCopyDir(reportPath) {
+  copyDir.sync(path.resolve(process.cwd(), reportPath), reportDir)
+}
 
 const walkSync = function(dir, filelist = []) {
   const files = fs.readdirSync(dir)
@@ -13,23 +16,29 @@ const walkSync = function(dir, filelist = []) {
     if(fs.statSync(path.join(dir, file)).isDirectory()) {
       filelist = walkSync(path.join(dir, file), filelist)
     }
-    else {filelist.push(path.join(dir, file))}
+    else {
+      filelist.push(path.join(dir, file))
+    }
   })
   return filelist
 }
 
-function getSuitFiles() {return walkSync(reportDir).filter(function(file) {return file.includes('-suit.json')})}
+function getSuitFiles() {
+  return walkSync(reportDir).filter((file) => file.includes('-suit.json'))
+}
 
 
 function mapStepsToRequiredData(steps, file) {
   const dir = path.dirname(file)
-  return steps.map(function(step) {
-    const title = step.title
-    const attachments = step.files.map(function(file) {
+  return steps.map((step) => {
+    const {title} = step
+    const attachments = step.files.map((file) => {
       const ext = path.extname(path.resolve(dir, file))
       if(!fs.existsSync('./src/images')) {fs.mkdirSync('./src/images')}
       if(ext === '.png') {
-        fs.createReadStream(path.resolve(dir, file)).pipe(fs.createWriteStream(path.resolve(__dirname, `./src/images/${file}`)))
+        fs.createReadStream(path.resolve(dir, file)).pipe(
+          fs.createWriteStream(path.resolve(__dirname, `./src/images/${file}`))
+        )
         return {img: `./images/${file}`}
       } else if(ext === '.json') {
         return {json: require(path.resolve(dir, file))}
@@ -45,38 +54,43 @@ function mapStepsToRequiredData(steps, file) {
 const testStackIncludes = ['failed', 'broken']
 
 function getSuitToData(suitData, file) {
-  const title = suitData.title
-  const status = suitData.status
-  const browser = suitData.browser
+  const {title, status, browser} = suitData
 
   const hooks = suitData.hooks.map(function(hook) {
-    const title = hook.title
-    const start = hook.timeStart
-    const end = hook.timeEnd
-    const state = hook.state
-    const duration = hook.duration
+    const {state, title, timeStart: start, timeEnd: end, duration} = hook
     const steps = mapStepsToRequiredData(hook.steps, file)
 
-    return {title, start, state, end, duration, steps, errorStack: testStackIncludes.includes(state) ? hook.errorStack.stack : undefined}
+    return {
+      title,
+      start,
+      state,
+      end,
+      duration,
+      steps,
+      errorStack: testStackIncludes.includes(state) ? hook.errorStack.stack : undefined
+    }
   })
 
   const tests = suitData.tests.map(function(test) {
-    const title = test.title
-    const start = test.timeStart
-    const end = test.timeEnd
-    const testOptions = test.testOptions
-    const state = test.state
-    const duration = test.duration
+    const {title, timeStart: start, timeEnd: end, testOptions, state, duration} = test
     const steps = mapStepsToRequiredData(test.steps, file)
-
-    return {title, start, state, end, duration, steps, testOptions, errorStack: testStackIncludes.includes(state) ? test.errorStack.stack : undefined}
-
+    return {
+      title,
+      start,
+      state,
+      end,
+      duration,
+      steps,
+      testOptions,
+      errorStack: testStackIncludes.includes(state) ? test.errorStack.stack : undefined
+    }
   })
+
   return {title, status, hooks, tests, browser}
 }
 
 function putBaseFile() {
-  const data = getSuitFiles().map(function(file) {
+  const data = getSuitFiles().map((file) => {
     const lastDir = path.dirname(file).split('/')
     const JSON_DATA = require(file)
     // need for consistency
@@ -94,14 +108,9 @@ function putBaseFile() {
       passed: JSON_DATA.stats.passes,
       failed: JSON_DATA.stats.failures
     }
-    const runName = JSON_DATA.runName
-    const opts = JSON_DATA.opts
-    const title = JSON.title
-
-    const suits = JSON_DATA.suits.map(function(suit) {
-      return getSuitToData(suit, file)
-    })
-    return {stats, runName, suits, opts, dirDate: lastDir[lastDir.length - 1]}
+    const {runName, opts, title} = JSON_DATA
+    const suits = JSON_DATA.suits.map((suit) => getSuitToData(suit, file))
+    return {title, stats, runName, suits, opts, dirDate: lastDir[lastDir.length - 1]}
   })
   if(fs.existsSync(path.resolve(__dirname, './src/reducers/base.json'))) {
     fs.unlinkSync(path.resolve(__dirname, './src/reducers/base.json'))
